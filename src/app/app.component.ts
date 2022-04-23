effdc53855681c9104e526b054600bc08d1cba57
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-04-23 17:22:46 -0400

    feat: method implementation to remove task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,20 @@ export class AppComponent {
     this.todos.push(new Todo(2, "Estudar uma hora de Angular 2 com Typescript", false));
     this.todos.push(new Todo(3, "Concluir o primeiro módulo do curso de Angular", false));
   }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if(index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {
+
+  }
+
+  markAsUndone() {
+
+  }
 }
